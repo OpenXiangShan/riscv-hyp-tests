7d0186ac4723a72771da84a6b95e8b6341a37ae6
Zhaoyang You <45031352+sinceforYy@users.noreply.github.com>
2025-09-01 10:09:02 +0800

    fix(ip,ie): fix xip, xie test (#8)

--- test_register.c
+++ test_register.c
@@ -4,8 +4,8 @@
 TEST_REGISTER_HYP(two_stage_translation);
 TEST_REGISTER_HYP(second_stage_only_translation);
 TEST_REGISTER_HYP(m_and_hs_using_vs_access);
-// TEST_REGISTER_HYP(check_xip_regs); //need that xiangshan run it alone
-// TEST_REGISTER_HYP(check_xie_regs); //need that xiangshan run it alone
+TEST_REGISTER_HYP(check_xip_regs);
+TEST_REGISTER_HYP(check_xie_regs);
 TEST_REGISTER_HYP(interrupt_tests);
 TEST_REGISTER_HYP(virtual_instruction);
 // TEST_REGISTER_HYP(hfence_test); //need that xiangshan run it alone
